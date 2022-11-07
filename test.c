d075b2cec0d9d063b3e2b1bf5e8921539e2001cf
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-11-07 21:47:14 +1000

    fix

--- test.c
+++ test.c
@@ -10,61 +10,61 @@
 #include "property_set.h"
 #include "debug.h"
 
-#include <iconv.h>
+/*#include <iconv.h>*/
 #include <stdint.h>
 #include <stdio.h>
 
-static char* unicode_decode_iconv(const char *s, size_t len, iconv_t ic) {
-	char* outbuf = 0;
+/*static char* unicode_decode_iconv(const char *s, size_t len, iconv_t ic) {*/
+	/*char* outbuf = 0;*/
 
-	if(s && len && ic)
-	{
-		size_t outlenleft = len;
-		int outlen = len;
-		size_t inlenleft = len;
-		const char* src_ptr = s;
-		char* out_ptr = 0;
+	/*if(s && len && ic)*/
+	/*{*/
+		/*size_t outlenleft = len;*/
+		/*int outlen = len;*/
+		/*size_t inlenleft = len;*/
+		/*const char* src_ptr = s;*/
+		/*char* out_ptr = 0;*/
 
-		size_t st; 
-		outbuf = malloc(outlen + 1);
+		/*size_t st; */
+		/*outbuf = malloc(outlen + 1);*/
 
-		if(outbuf)
-		{
-			out_ptr = outbuf;
-			while(inlenleft)
-			{
-				st = iconv(ic, (char **)&src_ptr, &inlenleft, (char **)&out_ptr,(size_t *) &outlenleft);
-				if(st == (size_t)(-1))
-				{
-					if(errno == E2BIG)
-					{
-						size_t diff = out_ptr - outbuf;
-						outlen += inlenleft;
-						outlenleft += inlenleft;
-						outbuf = realloc(outbuf, outlen + 1);
-						if(!outbuf)
-						{
-							break;
-						}
-						out_ptr = outbuf + diff;
-					}
-					else
-					{
-						free(outbuf), outbuf = NULL;
-						break;
-					}
-				}
-			}
-		}
-		outlen -= outlenleft;
+		/*if(outbuf)*/
+		/*{*/
+			/*out_ptr = outbuf;*/
+			/*while(inlenleft)*/
+			/*{*/
+				/*st = iconv(ic, (char **)&src_ptr, &inlenleft, (char **)&out_ptr,(size_t *) &outlenleft);*/
+				/*if(st == (size_t)(-1))*/
+				/*{*/
+					/*if(errno == E2BIG)*/
+					/*{*/
+						/*size_t diff = out_ptr - outbuf;*/
+						/*outlen += inlenleft;*/
+						/*outlenleft += inlenleft;*/
+						/*outbuf = realloc(outbuf, outlen + 1);*/
+						/*if(!outbuf)*/
+						/*{*/
+							/*break;*/
+						/*}*/
+						/*out_ptr = outbuf + diff;*/
+					/*}*/
+					/*else*/
+					/*{*/
+						/*free(outbuf), outbuf = NULL;*/
+						/*break;*/
+					/*}*/
+				/*}*/
+			/*}*/
+		/*}*/
+		/*outlen -= outlenleft;*/
 
-		if(outbuf)
-		{
-			outbuf[outlen] = 0;
-		}
-	}
-	return outbuf;
-}
+		/*if(outbuf)*/
+		/*{*/
+			/*outbuf[outlen] = 0;*/
+		/*}*/
+	/*}*/
+	/*return outbuf;*/
+/*}*/
 
 int prop_cb(void * user_data, uint32_t propid, uint32_t dwType, uint32_t * value){
 	char * str = NULL;
@@ -73,9 +73,9 @@ int prop_cb(void * user_data, uint32_t propid, uint32_t dwType, uint32_t * value
 		char buf[BUFSIZ];
 		strncpy(buf, (char*)value + 4, BUFSIZ);
 		//str = cprecode(buf, CPRECODE_CP1251, CPRECODE_UTF8);
-		//str = buf;
-		iconv_t ic = iconv_open("UTF-8", "CP1251");
-		str = unicode_decode_iconv(buf, strlen(buf), ic);
+		str = buf;
+		/*iconv_t ic = iconv_open("UTF-8", "CP1251");*/
+		/*str = unicode_decode_iconv(buf, strlen(buf), ic);*/
 	}
 	else if (dwType == 2){
 		char buf[BUFSIZ];
